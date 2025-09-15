ac71f28a1ebe1057bf9fd2124a519761452a9d54
William <2246747@cstj.qc.ca>
2025-09-14 22:51:42 -0400

    refactor(FormBuilder): Use Pinia store for core form management

--- src/pages/FormBuilder.vue
+++ src/pages/FormBuilder.vue
@@ -3,42 +3,22 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import BuilderFormElementsSidebar from "@/components/BuilderFormElementsSidebar.vue";
 import BuilderFormElementsPropertiesSidebar from "@/components/BuilderFormElementsPropertiesSidebar.vue";
 import FormElement from "@/components/FormElement.vue";
-import { provide, ref } from 'vue';
-import type { FormElement as FormElementType } from '@/types/form';
+
 import Draggable from 'vuedraggable';
 
-const previewForm = ref<FormElementType[]>([]);
-const selectedElementId = ref<string | null>(null);
+import { useFormBuilderStore } from '@/stores/FormBuilderStore'
 
-provide('previewForm', previewForm);
-provide('selectedElementId', selectedElementId);
+const store = useFormBuilderStore()
 
-const addElement = (element: FormElementType) => {
-  console.log('Adding element:', element);
-  previewForm.value.push({ ...element });
-};
 
-const selectElement = (elementId: string) => {
-  selectedElementId.value = elementId;
-  console.log('Selected element ID:', selectedElementId.value);
-};
 
-const deleteElement = (elementId: string) => {
-  previewForm.value = previewForm.value.filter(e => e.id !== elementId);
-  if (selectedElementId.value === elementId) {
-    selectedElementId.value = null;
-  }
-};
 
-const isElementSelected = (elementId: string) => {
-  return selectedElementId.value === elementId;
-};
 </script>
 
 
 <template>
   <SidebarProvider style="--sidebar-width: 20rem; --sidebar-width-mobile: 20rem;">
-    <BuilderFormElementsSidebar @add-element="addElement" />
+    <BuilderFormElementsSidebar  />
     
     <div class="flex flex-col items-center mx-auto w-5/10 h-full p-4">
       <h1 class="text-2xl font-semibold mb-4">Form preview</h1>
@@ -48,7 +28,7 @@ const isElementSelected = (elementId: string) => {
         aria-label="Form preview scrollable area"
       >
         <p 
-          v-if="previewForm.length === 0" 
+          v-if="store.elementCount === 0" 
           class="text-gray-500 dark:text-gray-400 absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-center"
         >
           Drag and drop form elements here
@@ -61,7 +41,7 @@ const isElementSelected = (elementId: string) => {
           appear
         >
           <Draggable
-            v-model="previewForm"
+            v-model="store.elementsForVModel"
             group="form-elements"
             item-key="id"
             handle=".drag-handle"
@@ -72,9 +52,8 @@ const isElementSelected = (elementId: string) => {
               <FormElement
                 :key="element.id"
                 :element="element"
-                :is-selected="isElementSelected(element.id)"
-                @select="selectElement"
-                @delete="deleteElement"
+                
+                
               />
             </template>
           </Draggable>
