bf1ff6d40593cbd4c4a20d4a5692dddd72fc2099
William <2246747@cstj.qc.ca>
2025-09-14 22:51:42 -0400

    feat: Setup Pinia for global state management

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import '@/style.css'
 import App from '@/App.vue'
 import router from '@/router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(router)
+app.use(pinia)
 app.mount('#app')
